6105e91e99995744985c03bbb542033c830bfa18
Takeaki Miyazawa <tmiyazawa@netprotections.co.jp>
2022-12-01 21:29:13 +0900

    first commit

---
+++ lua/options.lua
@@ -0,0 +1,14 @@
+vim.opt.showmatch = true    -- show matching brackets
+vim.opt.ignorecase = true   -- case insensitive matching
+-- vim.opt.mouse="v"   -- middle-click paste with mouse
+vim.opt.hlsearch = true   -- highlight search results
+vim.opt.autoindent = true   -- indent a new line the same amount as the line just typed
+vim.opt.number = true -- add line number
+-- vim.opt.wildmode={"longest""list"} -- get bash-like tab bompletions
+-- vim.opt.--set cc=88   -- set colour columns for good coding style
+-- vim.opt.filetype plugin indent on  -- allows auto-indenting depending on file type TODO IF ERROR OCCURRED COMMENT OUT
+vim.opt.tabstop=2   -- number of columns occupied by a tab character
+vim.opt.expandtab = true   -- convert tabs to white space
+vim.opt.shiftwidth=2  -- width for autoindents
+vim.opt.softtabstop=4  -- see multiple spaces as tabstops so <BS> does the right things
+
